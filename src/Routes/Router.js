21942b8f783cd291c0b069ca8f2c16d8137b780a
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-24 14:01:09 +0600

    Added Demo Homepage to the Router.

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,6 +1,17 @@
+import Home from '../Pages/Home/Home';
+
 const { createBrowserRouter } = require('react-router-dom');
 const { default: MainLayout } = require('../Layouts/MainLayout/MainLayout');
 
-const router = createBrowserRouter([{ path: '/', element: <MainLayout /> }]);
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <MainLayout />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: 'home', element: <Home /> },
+		],
+	},
+]);
 
 export default router;
