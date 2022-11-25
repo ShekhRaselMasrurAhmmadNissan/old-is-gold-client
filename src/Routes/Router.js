efb3e419efb08c7f36c68b872429f998e4115aaa
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-25 23:23:04 +0600

    Added error Element to the route.

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -1,3 +1,4 @@
+import Error from '../Pages/Error/Error';
 import Home from '../Pages/Home/Home';
 import Login from '../Pages/Login/Login';
 import Register from '../Pages/Register/Register';
@@ -9,6 +10,7 @@ const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <MainLayout />,
+		errorElement: <Error />,
 		children: [
 			{ index: true, element: <Home /> },
 			{ path: 'home', element: <Home /> },
