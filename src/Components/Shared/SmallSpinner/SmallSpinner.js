8f5e538afddfa57de7c8ff222ea8302cb35d129c
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-25 21:48:02 +0600

    Created Small Spinner.

---
+++ src/Components/Shared/SmallSpinner/SmallSpinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { PropagateLoader } from 'react-spinners';
+
+const SmallSpinner = () => {
+	return <PropagateLoader color="#36d7b7" className="mb-6" />;
+};
+
+export default SmallSpinner;
