48e427b86342a69367cfd84521e2d7fa7e3bcf32
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-25 16:26:59 +0600

    Created Login page.

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,91 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import SocialLogin from '../../Components/Shared/SocialLogin/SocialLogin';
+import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
+	const [error, setError] = useState('');
+	const { login } = useContext(AuthContext);
+	const { register, handleSubmit } = useForm();
+
+	const location = useLocation();
+	const navigate = useNavigate();
+	const from = location.state?.from?.pathname || '/home';
+	const handleLogin = async (data) => {
+		console.log(data);
+		try {
+			const response = await login(data.email, data.password);
+			console.log(response.user);
+			const currentUser = { email: response.user.email };
+			// const tokenResponse = await axios.post(
+			// 	`https://flawless-visa-server.vercel.app/jwt`,
+			// 	currentUser
+			// );
+			// console.log(tokenResponse.data);
+			// localStorage.setItem(
+			// 	'flawless-visa-token',
+			// 	tokenResponse.data.token
+			// );
+			navigate(from, { replace: true });
+		} catch (error) {
+			console.error(error);
+			setError(error.message);
+		}
+	};
 	return (
-		<div>
-			<h2 className="text-3xl">This is Login Page.</h2>
+		<div className="w-full max-w-md p-8 space-y-3 rounded-xl bg-gray-200 mt-8 md:mx-auto text-gray-800">
+			<h1 className="text-3xl font-bold text-center text-blue-500">
+				Login
+			</h1>
+			<form
+				onSubmit={handleSubmit(handleLogin)}
+				action=""
+				className="space-y-6 ng-untouched ng-pristine ng-valid"
+			>
+				<div className="space-y-1 text-sm">
+					<label htmlFor="email" className="block text-gray-600">
+						Email
+					</label>
+					<input
+						type="email"
+						{...register('email')}
+						id="email"
+						placeholder="Email"
+						className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800 outline-none"
+						required
+					/>
+				</div>
+				<div className="space-y-1 text-sm">
+					<label htmlFor="password" className="block text-gray-600">
+						Password
+					</label>
+					<input
+						type="password"
+						{...register('password')}
+						id="password"
+						placeholder="Password"
+						className="w-full px-4 py-3 rounded-md border-gray-300 bg-gray-50 text-gray-800 outline-none"
+						required
+					/>
+				</div>
+				{error && (
+					<p className="text-md font-medium text-red-500">{error}</p>
+				)}
+				<button
+					type="submit"
+					className="block w-full p-3 text-center text-lg text-gray-50 bg-blue-600 rounded-lg"
+				>
+					Sign in
+				</button>
+			</form>
+			<SocialLogin />
+			<p className="text-sm text-center sm:px-6 text-gray-600">
+				Don't have an account?{' '}
+				<Link to="/register" className="underline text-blue-800">
+					Sign up
+				</Link>
+			</p>
 		</div>
 	);
 };
