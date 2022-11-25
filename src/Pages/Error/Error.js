d377da02b681a647cbe893f289ec9bbbb6f08352
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-25 23:25:29 +0600

    Edited the design of Error Element.

--- src/Pages/Error/Error.js
+++ src/Pages/Error/Error.js
@@ -5,9 +5,12 @@ import { Link } from 'react-router-dom';
 
 const Error = () => {
 	return (
-		<div>
-			<Lottie animationData={error} className="w-4/5 mx-auto h-[650px]" />
-			<div className="text-center">
+		<div className="mt-6">
+			<Lottie
+				animationData={error}
+				className="w-4/5 mx-auto lg:h-[650px]"
+			/>
+			<div className="text-center mt-2">
 				<Link
 					to="/home"
 					className="bg-blue-600 text-white text-lg px-3 py-1 rounded-md font-medium"
