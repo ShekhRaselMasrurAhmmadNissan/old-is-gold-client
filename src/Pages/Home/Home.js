1043821ea2bfbb7c3a3fda31a58bfcf80b3c6dde
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-26 20:32:26 +0600

    Added WhyUs section to the Home page.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../../Components/Home/Banner/Banner';
+import WhyUs from '../../Components/Home/WhyUS/WhyUs';
 
 const Home = () => {
 	return (
 		<div>
 			<Banner />
+			<h2 className="text-3xl">Advertise section</h2>
+			<h2 className="text-3xl">Categories.</h2>
+			<WhyUs />
 		</div>
 	);
 };
