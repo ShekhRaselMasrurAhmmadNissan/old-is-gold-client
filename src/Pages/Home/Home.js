8077efc190c2e108b383a1d88e8bfe2fca6025d3
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-26 12:23:09 +0600

    Added Banner to the Home component.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../../Components/Home/Banner/Banner';
 
 const Home = () => {
 	return (
 		<div>
-			<h2 className="text-3xl">This is Home Page.</h2>
+			<Banner />
 		</div>
 	);
 };
