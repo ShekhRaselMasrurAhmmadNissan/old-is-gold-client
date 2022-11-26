030aa7340993a3f94ef84e7322daf7791f8cc4a5
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2022-11-27 00:43:00 +0600

    Edited Home design.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,7 +4,7 @@ import WhyUs from '../../Components/Home/WhyUS/WhyUs';
 
 const Home = () => {
 	return (
-		<div>
+		<div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl lg:px-4">
 			<Banner />
 			<h2 className="text-3xl">Advertise section</h2>
 			<h2 className="text-3xl">Categories.</h2>
